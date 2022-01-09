8d74708fe5ec5fd7826ad08428760df76b5cea78
ismailkefokeris <ismailkefokeris@gmail.com>
2022-01-09 19:05:03 +0000

    Javadoc

--- model/src/main/java/org/solent/com504/oodd/cart/model/dto/Address.java
+++ model/src/main/java/org/solent/com504/oodd/cart/model/dto/Address.java
@@ -2,6 +2,10 @@ package org.solent.com504.oodd.cart.model.dto;
 
 import javax.persistence.Embeddable;
 
+/**
+ *
+ * @author ismai
+ */
 @Embeddable
 public class Address {
 
@@ -23,75 +27,146 @@ public class Address {
 
     private String country;
 
+    /**
+     *
+     * @return
+     */
     public String getHouseNumber() {
         return houseNumber;
     }
 
+    /**
+     *
+     * @param houseNumber
+     */
     public void setHouseNumber(String houseNumber) {
         this.houseNumber = houseNumber;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getAddressLine1() {
         return addressLine1;
     }
 
+    /**
+     *
+     * @param addressLine1
+     */
     public void setAddressLine1(String addressLine1) {
         this.addressLine1 = addressLine1;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getAddressLine2() {
         return addressLine2;
     }
 
+    /**
+     *
+     * @param addressLine2
+     */
     public void setAddressLine2(String addressLine2) {
         this.addressLine2 = addressLine2;
     }
 
-
+    /**
+     *
+     * @return
+     */
     public String getPostcode() {
         return postcode;
     }
 
+    /**
+     *
+     * @param postcode
+     */
     public void setPostcode(String postcode) {
         this.postcode = postcode;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMobile() {
         return mobile;
     }
 
+    /**
+     *
+     * @param mobile
+     */
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getTelephone() {
         return telephone;
     }
 
+    /**
+     *
+     * @param telephone
+     */
     public void setTelephone(String telephone) {
         this.telephone = telephone;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCountry() {
         return country;
     }
 
+    /**
+     *
+     * @param country
+     */
     public void setCountry(String country) {
         this.country = country;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCounty() {
         return county;
     }
 
+    /**
+     *
+     * @param county
+     */
     public void setCounty(String county) {
         this.county = county;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     *
+     * @param city
+     */
     public void setCity(String city) {
         this.city = city;
     }
